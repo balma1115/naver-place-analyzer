39baef6f1d72c5175246096456bc7099cc96944a
balma1115 <superysm@gmail.com>
2025-07-18 15:24:11 +0900

    Add files via upload

--- start.sh
+++ start.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
+
+# === 디버깅을 위한 코드 추가 ===
+echo "=================================="
+echo "Attempting to start server on PORT: $PORT"
+echo "=================================="
+# ===============================
+
 uvicorn main:app --host 0.0.0.0 --port $PORT
